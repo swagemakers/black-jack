6c9c88c3ca5321a7290b4941e903444557fcfa48
Kill la kill <swagemakers.mp@gmail.com>
2019-06-13 14:41:18 +0300

    fff

--- cards.rb
+++ cards.rb
@@ -1,20 +1,36 @@
+RANK = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]
+SUIT = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
 class Cards
+
+  attr_reader :rank, :suit
+  attr_accessor :value
 
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
   end
 
-  def card_value(rank)
-    case value do
-    when 1
-      then 1
-    when 2..10
-      then value
-    when 11..13
-      then 10
+  def card_value
+    value = case @rank.to_i
+      when 1 then 11
+      when 2..10 then value
+      when 11..13 then 10
+      end
     end
   end
+
+  def display_rank
+    case @rank
+      when "1" then "Ace"
+      when "11" then "Jack"
+      when "12" then "Queen"
+      when "13" then "King"
+      when 2..10 then @rank
+      end
+    end
+  end
+
+  def to_s
+    "#{display_rank} of #{suit}'s value is #{value}"
+  end
 end
-
-
