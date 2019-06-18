f443b8518079eca5721f3b62ccee93c5799003dd
swagemakers <48688649+swagemakers@users.noreply.github.com>
2019-06-18 15:38:41 +0300

    git left chat

--- player.rb
+++ player.rb
@@ -1,9 +1,12 @@
-class Player < Main_player
+class Dealer < Main_player
+
+  DEALER = 'DEALER'
+
   attr_accessor :cards, :bank, :points
   attr_reader :name
 
-  def initialize(name)
-    @name = name
+  def initialize(name = DEALER)
+    @name = DEALER
     @value = value
     @cards = []
     super
