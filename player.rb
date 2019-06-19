6b2fc433fdf2459674eb87da26c681c8a5e14762
Kill la kill <swagemakers.mp@gmail.com>
2019-06-19 18:52:22 +0300

    fnig merging going on

--- player.rb
+++ player.rb
@@ -1,11 +1,11 @@
-class Player < Main_player
+class Player
   attr_accessor :cards, :bank, :points
   attr_reader :name
 
   def initialize(name)
     @name = name
-    @value = value
+    @bank = 100
+    @value = 0
     @cards = []
-    super
   end
 end
