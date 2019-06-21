871b787727491ae21645c4067b8e3d06bd2e852b
Kill la kill <swagemakers.mp@gmail.com>
2019-06-21 16:36:33 +0300

    update

--- game.rb
+++ game.rb
@@ -15,5 +15,29 @@ attr_accessor :deck
     2.times { @players_hand.get_card(@deck) }
     2.times { @dealer_hand.get_card(@deck) }
   end
+
+  #here comes the loop for all the input, make the bank work!
+
+  def hit!
+    @players_hand.get_card(deck)
+  end
+
+  def stand
+    @dealer_hand.dealers_move(@deck)
+    @winner = determine_winner(@players_hand.points, @dealer_hand.points)
+  end
+
+  def determine_winner(@players_hand.points, @dealer_hand.points)
+    if @player_hand.value > 21
+      puts "Dealer has won this round"
+    elsif @dealer_hand.value  > 21
+      puts "#{name} has won this round"
+    end
+    #two more options go in here
+  end
+
+  def status
+    puts "#{name} cards and value are: #{@player.cards}, #{@hand.points}"
+  end
 end
 
